77050fa42a048a66b4621b020216f35985d37c9b
Kseniia Smirnova <115711811+KseniiaSmirn0va@users.noreply.github.com>
2024-01-30 14:50:03 +0300

    Update medium.php

--- dvwa/vulnerabilities/csp/source/medium.php
+++ dvwa/vulnerabilities/csp/source/medium.php
@@ -7,7 +7,7 @@ header($headerCSP);
 // Disable XSS protections so that inline alert boxes will work
 header ("X-XSS-Protection: 0");
 # <script nonce="TmV2ZXIgZ29pbmcgdG8gZ2l2ZSB5b3UgdXA=">alert(1)</script>
-// <script nonce="TmV2ZXIgZ29pbmcgdG8gZ2l2ZSB5b3UgdXA=">alert(1)</script>
+// <blabla nonce="TmV2ZXIgZ29pbmcgdG8gZ2l2ZSB5b3UgdXA=">alert(1)</blabla>
 # nonce="TmV2ZXIgZ29pbmcgdG8gZ2l2ZSB5b3UgdXA="
 <script nonce="TmV2ZXIgZ29pbmcgdG8gZ2l2ZSB5b3UgdXA=">alert(1)</script>
 
